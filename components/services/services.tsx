f540fda61f4857f67edc68f8020647dfb2b43d0f
Michal-Kilian <kilian.michal.kili@gmail.com>
2025-05-15 00:23:21 +0200

    tsc fixes

--- components/services/services.tsx
+++ components/services/services.tsx
@@ -18,29 +18,4 @@ export const Services = () => {
       </BentoGrid>
     </section>
   );
-
-  {/*return (
-    <section
-      id="servicesSection"
-      className='flex w-full flex-col items-center justify-center p-6'
-    >
-      <Card className='w-full'>
-        <CardHeader>
-          <CardTitle className='text-primary flex w-full flex-row items-center justify-start gap-x-2 text-left text-3xl font-medium tracking-tight'>
-            <Package />
-            Služby
-          </CardTitle>
-          <CardDescription className='text-primary w-full text-left'>
-            Vyberte si z našich komplexných služieb v oblasti výstavby,
-            demolácií a technickej infraštruktúry.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className='flex w-full flex-1 flex-row flex-wrap items-center justify-evenly gap-6 px-6 transition-all duration-200'>
-          {SERVICES.map((service: Service) => (
-            <ServiceCard key={service.id} service={service} />
-          ))}
-        </CardContent>
-      </Card>
-    </section>
-  );*/}
 };
