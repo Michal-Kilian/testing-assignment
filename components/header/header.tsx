ac598b18951c1996d7d6e615a92c98487b1d141c
Michal-Kilian <kilian.michal.kili@gmail.com>
2025-05-16 12:39:11 +0200

    format

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -12,13 +12,13 @@ export const Header = (): ReactElement => {
   return (
     <header className='bg-primary text-primary-foreground flex h-fit w-full flex-row items-center justify-between p-2'>
       <Link href='#' className='w-full'>
-        <div className='flex w-full cursor-pointer flex-row items-center justify-start gap-x-3 ml-1.5'>
+        <div className='ml-1.5 flex w-full cursor-pointer flex-row items-center justify-start gap-x-3'>
           <Image
-            src="/logo.png"
-            alt="Logo"
+            src='/logo.png'
+            alt='Logo'
             width={100}
             height={100}
-            className="w-6 h-6 border border-background rounded-full"
+            className='border-background h-6 w-6 rounded-full border'
           />
           <h1 className='w-fit font-medium tracking-tight'>
             Blueprint Solutions s.r.o.
